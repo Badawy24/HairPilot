16ce0b0bbfd0f0bb3f7538e4b00985b7f9bde68a
Badawy24 <abdelrahman.ahmed2410@gmail.com>
2025-06-09 20:15:27 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
+import { FooterComponent } from "./footer/footer.component";
+import { GalleryComponent } from "./gallery/gallery.component";
+import { PriceComponent } from "./price/price.component";
+import { ServiceComponent } from "./service/service.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, AboutComponent, ContactComponent, FooterComponent, GalleryComponent, PriceComponent, ServiceComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'HairPilot';
+}
